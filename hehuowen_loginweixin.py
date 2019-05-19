71fb522a9002a31e24a7fa2279afa13cdab2b57f
贺火文 <48775706+Wen1203054281@users.noreply.github.com>
2019-05-19 11:08:02 +0800

    Add files via upload

---
+++ hehuowen_loginweixin.py
@@ -0,0 +1,14 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:/Users/Admin/Desktop/hehuowen_loginweixin.py ==========
+█Getting uuid of QR code.
+Downloading QR code.
+Please scan the QR code to log in.
+Please press confirm on your phone.
+Loading the contact, this may take a little while.
+Login successfully as 52Hertz
+男性好友：68.09%
+女性好友：25.53%
+其他：8.51%
+>>> 
